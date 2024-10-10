78555d245dca67ce38c6ce848ae87ffbcf4afdc9
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2024-10-10 20:47:38 -0300

    Update ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,7 +33,7 @@ return {
 
 			opts.commands = {
 				all = {
-					-- options for the message history that you get with `:Noice`
+					-- options for the message history that you get with :Noice
 					view = "split",
 					opts = { enter = true, format = "details" },
 					filter = {},
@@ -81,44 +81,11 @@ return {
 		},
 		opts = {
 			options = {
-				mode = "tabs",
-				-- separator_style = "slant",
+				separator_style = "slant",
 				show_buffer_close_icons = false,
-				show_close_icon = false,
+				show_close_icon = true,
 			},
 		},
-	},
-
-	-- filename
-	{
-		"b0o/incline.nvim",
-		dependencies = { "craftzdog/solarized-osaka.nvim" },
-		event = "BufReadPre",
-		priority = 1200,
-		config = function()
-			local colors = require("solarized-osaka.colors").setup()
-			require("incline").setup({
-				highlight = {
-					groups = {
-						InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-						InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-					},
-				},
-				window = { margin = { vertical = 0, horizontal = 1 } },
-				hide = {
-					cursorline = true,
-				},
-				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					if vim.bo[props.buf].modified then
-						filename = "[+] " .. filename
-					end
-
-					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-					return { { icon, guifg = color }, { " " }, { filename } }
-				end,
-			})
-		end,
 	},
 
 	-- statusline
