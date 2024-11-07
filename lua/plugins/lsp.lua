231f220c2a2f5f4e39c0c0cd2c0c3227ccccf8bc
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2024-11-07 00:50:03 -0300

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,14 @@
+local home = os.getenv("HOME")
+local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
 return {
+  -- `nvim-jdtls` plugin to program with Java
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = { "java" }, 
+
   -- tools
   {
     "williamboman/mason.nvim",
@@ -12,6 +22,7 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
+        "jdtls", 
       })
     end,
   },
@@ -68,7 +79,6 @@ return {
           },
         },
         lua_ls = {
-          -- enabled = false,
           single_file_support = true,
           settings = {
             Lua = {
@@ -79,78 +89,79 @@ return {
                 workspaceWord = true,
                 callSnippet = "Both",
               },
-              misc = {
-                parameters = {
-                  -- "--log-level=trace",
-                },
-              },
               hint = {
                 enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              type = {
-                castNumberToInteger = true,
-              },
-              diagnostics = {
-                disable = { "incomplete-signature-doc", "trailing-space" },
-                -- enable = false,
-                groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
-                },
-                groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
               },
               format = {
                 enable = false,
                 defaultConfig = {
                   indent_style = "space",
                   indent_size = "2",
-                  continuation_indent_size = "2",
                 },
               },
             },
           },
         },
       },
-      setup = {},
+      setup = {
+        -- jdtls configuration for java files
+        jdtls = function()
+          local status, jdtls = pcall(require, "jdtls")
+          if not status then return end
+
+          local config = {
+            cmd = {
+              "java",
+              "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+              "-Dosgi.bundles.defaultStartLevel=4",
+              "-Declipse.product=org.eclipse.jdt.ls.core.product",
+              "-Dlog.protocol=true",
+              "-Dlog.level=ALL",
+              "-Xmx1g",
+              "--add-modules=ALL-SYSTEM",
+              "--add-opens",
+              "java.base/java.util=ALL-UNNAMED",
+              "--add-opens",
+              "java.base/java.lang=ALL-UNNAMED",
+              "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+              "-jar",
+              vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+              "-configuration",
+              home .. "/.local/share/nvim/mason/packages/jdtls/config_mac",
+              "-data",
+              workspace_dir,
+            },
+            root_dir = jdtls.setup.find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+            settings = {
+              java = {
+                signatureHelp = { enabled = true },
+                extendedClientCapabilities = jdtls.extendedClientCapabilities,
+                maven = { downloadSources = true },
+                referencesCodeLens = { enabled = true },
+                references = { includeDecompiledSources = true },
+                inlayHints = { parameterNames = { enabled = "all" } },
+                format = { enabled = false },
+              },
+            },
+            init_options = { bundles = {} },
+          }
+
+          jdtls.start_or_attach(config)
+          -- Specific Keymaps for Java
+          vim.keymap.set("n", "<leader>co", "<Cmd>lua require'jdtls'.organize_imports()<CR>",
+            { desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>crv", "<Cmd>lua require('jdtls').extract_variable()<CR>",
+            { desc = "Extract Variable" })
+          vim.keymap.set("v", "<leader>crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
+            { desc = "Extract Variable" })
+          vim.keymap.set("n", "<leader>crc", "<Cmd>lua require('jdtls').extract_constant()<CR>",
+            { desc = "Extract Constant" })
+          vim.keymap.set("v", "<leader>crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
+            { desc = "Extract Constant" })
+          vim.keymap.set("v", "<leader>crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
+            { desc = "Extract Method" })
+        end,
+      },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      vim.list_extend(keys, {
-        {
-          "gd",
-          function()
-            -- DO NOT RESUSE WINDOW
-            require("telescope.builtin").lsp_definitions({ reuse_win = false })
-          end,
-          desc = "Goto Definition",
-          has = "definition",
-        },
-      })
-    end,
   },
 }
