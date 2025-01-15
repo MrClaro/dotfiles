73a4213621351879465cfeb1f41e69843eae1028
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2025-01-15 20:00:34 -0300

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,13 +4,13 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = workspace_path .. project_name
 
 return {
-  -- Adds nvim-jdtls as a plugin to load only for Java files
+  -- nvim-jdtls plugin for Java files
   {
     "mfussenegger/nvim-jdtls",
     ft = { "java" }, -- Loads only for Java files
   },
 
-  -- Tools
+  -- Tools setup with Mason
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
@@ -23,23 +23,22 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
-        "html-lsp", -- Adds HTML LSP to Mason
+        "html-lsp",
         "emmet-ls", -- Emmet for HTML/CSS/JSX snippets
         "jdtls", -- Adds jdtls to Mason
       })
     end,
   },
 
-  -- LSP servers
+  -- LSP setup
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
-      ---@type lspconfig.options
       servers = {
         cssls = {},
-        html = {}, -- Enables HTML language server
-        emmet_ls = {}, -- Enables Emmet language server
+        html = {},
+        emmet_ls = {},
         tailwindcss = {
           root_dir = function(...)
             return require("lspconfig.util").root_pattern(".git")(...)
@@ -54,61 +53,36 @@ return {
             typescript = {
               inlayHints = {
                 includeInlayParameterNameHints = "literal",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
                 includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
               },
             },
             javascript = {
               inlayHints = {
                 includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
                 includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
               },
             },
           },
         },
         yamlls = {
           settings = {
-            yaml = {
-              keyOrdering = false,
-            },
+            yaml = { keyOrdering = false },
           },
         },
         lua_ls = {
           single_file_support = true,
           settings = {
             Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                workspaceWord = true,
-                callSnippet = "Both",
-              },
-              hint = {
-                enable = true,
-              },
-              format = {
-                enable = false,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "2",
-                },
-              },
+              workspace = { checkThirdParty = false },
+              completion = { workspaceWord = true, callSnippet = "Both" },
+              hint = { enable = true },
+              format = { enable = false, defaultConfig = { indent_style = "space", indent_size = "2" } },
             },
           },
         },
       },
       setup = {
-        -- JDTLS configuration for Java files
+        -- JDTLS setup for Java files
         jdtls = function()
           local status, jdtls = pcall(require, "jdtls")
           if not status then return end
@@ -123,17 +97,12 @@ return {
               "-Dlog.level=ALL",
               "-Xmx1g",
               "--add-modules=ALL-SYSTEM",
-              "--add-opens",
-              "java.base/java.util=ALL-UNNAMED",
-              "--add-opens",
-              "java.base/java.lang=ALL-UNNAMED",
+              "--add-opens", "java.base/java.util=ALL-UNNAMED",
+              "--add-opens", "java.base/java.lang=ALL-UNNAMED",
               "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-              "-jar",
-              vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-              "-configuration",
-              home .. "/.local/share/nvim/mason/packages/jdtls/config_linux", -- Adjusted for Linux
-              "-data",
-              workspace_dir,
+              "-jar", vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+              "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+              "-data", workspace_dir,
             },
             root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
             settings = {
@@ -150,20 +119,16 @@ return {
             init_options = { bundles = {} },
           }
 
+          -- Start or attach jdtls
           jdtls.start_or_attach(config)
-          -- Keymaps specific to Java
-          vim.keymap.set("n", "<leader>co", "<Cmd>lua require'jdtls'.organize_imports()<CR>",
-            { desc = "Organize Imports", buffer = true })
-          vim.keymap.set("n", "<leader>crv", "<Cmd>lua require('jdtls').extract_variable()<CR>",
-            { desc = "Extract Variable", buffer = true })
-          vim.keymap.set("v", "<leader>crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-            { desc = "Extract Variable", buffer = true })
-          vim.keymap.set("n", "<leader>crc", "<Cmd>lua require('jdtls').extract_constant()<CR>",
-            { desc = "Extract Constant", buffer = true })
-          vim.keymap.set("v", "<leader>crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-            { desc = "Extract Constant", buffer = true })
-          vim.keymap.set("v", "<leader>crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
-            { desc = "Extract Method", buffer = true })
+
+          -- Java-specific keymaps
+          vim.keymap.set("n", "<leader>co", "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = "Organize Imports", buffer = true })
+          vim.keymap.set("n", "<leader>crv", "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = "Extract Variable", buffer = true })
+          vim.keymap.set("v", "<leader>crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", { desc = "Extract Variable", buffer = true })
+          vim.keymap.set("n", "<leader>crc", "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = "Extract Constant", buffer = true })
+          vim.keymap.set("v", "<leader>crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = "Extract Constant", buffer = true })
+          vim.keymap.set("v", "<leader>crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = "Extract Method", buffer = true })
         end,
       },
     },
