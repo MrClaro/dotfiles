e6fabd1a11c371d06869f11ed2e52ae64c88fe3b
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2025-01-16 01:33:33 -0300

    Update integrations.lua

--- lua/plugins/integrations.lua
+++ lua/plugins/integrations.lua
@@ -41,4 +41,28 @@ return {
       },
     },
   },
+  -- LazyGit integration for Git commands
+  {
+    "kdheepak/lazygit.nvim",
+    keys = {
+      { ";c", ":LazyGit<Return>", silent = true, noremap = true },
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- Database management with vim-dadbod-ui
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    keys = {
+      { "<leader>d", "<cmd>NeoTreeClose<cr><cmd>tabnew<cr>|<cmd>DBUI<cr>", desc = "Open DBUI" },
+    },
+  },
 }
