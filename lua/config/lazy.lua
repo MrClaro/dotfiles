3f0d1a46c5c7fe3ebe72908f3a2424061e71389a
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2025-01-21 02:50:30 -0300

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,94 +1,62 @@
--- Set path for lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- Check if lazy.nvim is already installed, if not, clone it
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  
-  -- If cloning fails, show an error message and exit
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
-
--- Add lazy.nvim to runtime path
 vim.opt.rtp:prepend(lazypath)
 
--- Lazy plugin setup
 require("lazy").setup({
   spec = {
-    -- LazyVim plugin and configuration
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "catppuccin",  -- Use the 'catppuccin' theme
+        colorscheme = "catppuccin-mocha",
         news = {
           lazyvim = true,
           neovim = true,
         },
       },
     },
-    -- Linting and formatting plugins
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    
-    -- Utilities and extra plugins
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "plugins" },  -- Custom user plugins
+    { import = "plugins" },
   },
-
-  -- Default settings for plugins
   defaults = {
-    lazy = false,  -- Disable lazy-loading by default
-    version = false,  -- Always use the latest commit from plugins
+    lazy = false,
+    version = false,
   },
-
-  -- Development setup (path for custom development plugins)
   dev = {
     path = "~/.ghq/github.com",
   },
-
-  -- Plugin update checker
-  checker = { enabled = true },  -- Automatically check for updates
-
-  -- Performance optimizations
+  checker = { enabled = true },
   performance = {
-    cache = { enabled = true },  -- Enable cache for performance
+    cache = {
+      enabled = true,
+    },
     rtp = {
-      -- Disable certain runtime path plugins for faster startup
       disabled_plugins = {
-        "gzip",  -- Compression
-        "netrwPlugin",  -- File explorer
-        "rplugin",  -- Remote plugins
-        "tarPlugin",  -- TAR support
-        "tohtml",  -- HTML export
-        "tutor",  -- Tutor mode
-        "zipPlugin",  -- ZIP support
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
       },
     },
   },
-
-  -- UI customizations
-  ui = {
-    custom_keys = {
-      ["<localleader>d"] = function(plugin)
-        dd(plugin)  -- Custom keybinding for displaying plugin info
-      end,
-    },
-  },
-
-  -- Debugging settings
-  debug = false,  -- Disable debug logs
+  debug = false,
 })
