f50cbac32d31c9725758fc9cd84de4ddc6841262
Adryan Claro <133619633+MrClaro@users.noreply.github.com>
2025-01-21 02:51:33 -0300

    Update coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -98,23 +98,4 @@ return {
       position = "right",
     },
   },
-
-  -- Auto Complete code
-  {
-    "codota/tabnine-nvim",
-    build = "./dl_binaries.sh",
-    config = function()
-      require("tabnine").setup({
-        disable_auto_comment = true,
-        accept_keymap = "<Tab>",
-        dismiss_keymap = "<C-]>",
-        debounce_ms = 800,
-        suggestion_color = { gui = "#808080", cterm = 244 },
-        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-        log_file_path = nil,
-        ignore_certificate_errors = false,
-      })
-    end,
-  },
-
 }
