6bb90cb937fcc28440c94a5e294704be671270ec
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-16 17:51:54 -0300

    feat(java): adicionado plugins para trabalhar melhor com o java

--- .config/nvim/lua/plugins/languages/java.lua
+++ .config/nvim/lua/plugins/languages/java.lua
@@ -1,5 +1,4 @@
 return {
-
   -- Java dependencies helper
   {
     "JavaHello/java-deps.nvim",
@@ -31,5 +30,40 @@ return {
         vim.list_extend(opts.ensure_installed, { "java" })
       end
     end,
+  },
+
+  -- Spring Boot integration (JavaHello plugin)
+  {
+    "JavaHello/spring-boot.nvim",
+    ft = { "java", "yaml", "jproperties" },
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {},
+  },
+  {
+    "elmcgill/springboot-nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-jdtls",
+    },
+    config = function()
+      local springboot_nvim = require("springboot-nvim")
+      vim.keymap.set("n", "<leader>Ja", springboot_nvim.boot_run, { desc = "Spring Boot Run Project" })
+      vim.keymap.set("n", "<leader>Jc", springboot_nvim.generate_class, { desc = "Java Create Class" })
+      vim.keymap.set("n", "<leader>Ji", springboot_nvim.generate_interface, { desc = "Java Create Interface" })
+      vim.keymap.set("n", "<leader>Je", springboot_nvim.generate_enum, { desc = "Java Create Enum" })
+      vim.keymap.set("n", "<leader>Jr", springboot_nvim.generate_record, { desc = "Java Create Record" })
+      springboot_nvim.setup({})
+    end,
+  },
+  -- Java Genie: code generation and refactoring
+  {
+    "andreluisos/nvim-javagenie",
+    dependencies = {
+      "grapp-dev/nui-components.nvim",
+      "MunifTanjim/nui.nvim",
+    },
   },
 }
