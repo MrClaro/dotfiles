fc40d3a17565b441f72e1be12b4dc892399705f6
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-21 21:35:42 -0300

    feat_remove(plugins): adicionado novos plugins para melhorar o desenvolvimento e removido plugins não utilizados

--- .config/nvim/lua/plugins/integrations/functionalities.lua
+++ .config/nvim/lua/plugins/integrations/functionalities.lua
@@ -1,4 +1,41 @@
 return {
+  -- Flash navigation plugin
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "zk", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "Zk", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  -- Typst preview plugin
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "1.*",
+    opts = {
+      invert_colors = "always",
+    },
+    build = function()
+      require("typst-preview").update()
+    end,
+  },
+
+  -- Typst syntax support
+  {
+    "kaarmu/typst.vim",
+    ft = "typst",
+    enabled = not vim.g.vscode,
+    lazy = false,
+  },
+
+  -- Auto-close and rename HTML/XML tags
   {
     "windwp/nvim-ts-autotag",
     config = function()
@@ -9,7 +46,7 @@ return {
           enable_rename = true, -- Auto rename pairs of tags
           enable_close_on_slash = false, -- Auto close on trailing </
         },
-        -- Also override individual filetype configs, these take priority.
+        -- Override individual filetype configs, these take priority
         -- Empty by default, useful if one of the "opts" global settings
         -- doesn't work well in a specific filetype
         per_filetype = {
@@ -20,28 +57,87 @@ return {
       })
     end,
   },
-  {
-    "chomosuke/typst-preview.nvim",
-    lazy = false, -- or ft = 'typst'
-    version = "0.3.*",
-    build = function()
-      require("typst-preview").update()
-    end,
-  },
-  {
-    "kaarmu/typst.vim",
-    ft = "typst",
-    enabled = not vim.g.vscode,
-    lazy = false,
-  },
+
+  -- Remote Neovim development
   {
     "amitds1997/remote-nvim.nvim",
     version = "*", -- Pin to GitHub releases
     dependencies = {
       "nvim-lua/plenary.nvim", -- For standard functions
       "MunifTanjim/nui.nvim", -- To build the plugin UI
-      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+      "nvim-telescope/telescope.nvim", -- For picking between different remote methods
     },
     config = true,
+  },
+
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
+  -- Startup time measurement
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  -- Surround text objects
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0",
+    event = "VeryLazy", -- Fixed typo from "vent"
+    config = function()
+      require("nvim-surround").setup({
+        -- Define new mappings for Normal and Visual modes
+        keymaps = {
+          normal = "gs", -- Use 'gs' as the prefix for nvim-surround in Normal mode
+          -- Example: 'gs' + 'iw' + ')' to surround words
+          visual = "gS", -- Use 'gS' as the prefix for nvim-surround in Visual mode
+          -- Example: select text and then 'gS' + ')'
+          delete = "ds", -- Mapping to delete surround
+          change = "cs", -- Mapping to change surround
+        },
+      })
+    end,
+  },
+
+  -- Color highlighter
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        "*",
+        css = { rgb_fn = true },
+      })
+    end,
+  },
+
+  -- CSV file viewer
+  {
+    "hat0uma/csvview.nvim",
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        -- Text objects for selecting fields
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        -- Excel-like navigation:
+        -- Use <Tab> and <S-Tab> to move horizontally between fields.
+        -- Use <Enter> and <S-Enter> to move vertically between rows and place cursor at field end.
+        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
   },
 }
--- .config/nvim/lua/plugins/integrations/integrations.lua
+++ .config/nvim/lua/plugins/integrations/integrations.lua
@@ -87,103 +87,49 @@ return {
   },
 
   -- Quarto for Literate Programming
-  {
-    "quarto-dev/quarto-nvim",
-    dependencies = {
-      "jmbuhr/otter.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("quarto").setup()
-    end,
-    keys = {
-      name = "📚 Quarto",
-      { "<leader>qv", "<cmd>QuartoPreview<cr>", desc = "🖥️ Preview Quarto document" },
-      { "<leader>qc", "<cmd>QuartoClosePreview<cr>", desc = "❌ Close Quarto preview" },
-    },
-  },
+  -- {
+  --   "quarto-dev/quarto-nvim",
+  --   dependencies = {
+  --     "jmbuhr/otter.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   config = function()
+  --     require("quarto").setup()
+  --   end,
+  --   keys = {
+  --     name = "📚 Quarto",
+  --     { "<leader>qv", "<cmd>QuartoPreview<cr>", desc = "🖥️ Preview Quarto document" },
+  --     { "<leader>qc", "<cmd>QuartoClosePreview<cr>", desc = "❌ Close Quarto preview" },
+  --   },
+  -- },
 
   -- Comment code easily
-  {
-    "tpope/vim-commentary",
-    keys = {
-      name = "💬 Comment",
-      { "<leader>rc", "<cmd>Commentary<cr>", desc = "📝 Toggle comment" },
-    },
-  },
+  -- {
+  --   "tpope/vim-commentary",
+  --   keys = {
+  --     name = "💬 Comment",
+  --     { "<leader>rc", "<cmd>Commentary<cr>", desc = "📝 Toggle comment" },
+  --   },
+  -- },
 
   -- Database Management with DBUI
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-    end,
-    keys = {
-      name = "🗄 Database",
-      { "<leader>lb", "<cmd>DBUI<cr>", desc = "📂 Open DBUI" },
-      { "<leader>lB", "<cmd>DBUIToggle<cr>", desc = "🔄 Toggle DBUI" },
-      { "<leader>la", "<cmd>DBUIAddConnection<cr>", desc = "➕ Add new DB connection" },
-    },
-  },
-
-  -- HTTP Client Configuration
-  {
-    "rest-nvim/rest.nvim",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-    config = function()
-      require("rest-nvim").setup({
-        result_split_horizontal = false,
-        result_split_in_place = false,
-        stay_in_current_window_after_split = false,
-        skip_ssl_verification = false,
-        encode_url = true,
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          show_url = true,
-          show_curl_command = false,
-          show_http_info = true,
-          show_headers = true,
-          show_statistics = false,
-          formatters = {
-            json = "jq",
-            html = function(body)
-              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-            end,
-          },
-        },
-        jump_to_request = false,
-        env_file = ".env",
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
-        search_back = true,
-      })
-    end,
-    keys = {
-      {
-        "<leader>lr",
-        "<cmd>Rest open --url <cr>",
-        desc = "🌐 Test the current HTTP request",
-      },
-      {
-        "<leader>lt",
-        "<cmd>Rest cookies <ct>",
-        desc = "🍪 Edit the cookies for the current request",
-      },
-      {
-        "<leader>ls",
-        "<cmd>Rest run <cr>",
-        desc = "🚀 Run the current HTTP request",
-      },
-    },
-  },
+  -- {
+  --   "kristijanhusak/vim-dadbod-ui",
+  --   dependencies = {
+  --     { "tpope/vim-dadbod", lazy = true },
+  --     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  --   },
+  --   cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+  --   init = function()
+  --     vim.g.db_ui_use_nerd_fonts = 1
+  --   end,
+  --   keys = {
+  --     name = "🗄 Database",
+  --     { "<leader>lb", "<cmd>DBUI<cr>", desc = "📂 Open DBUI" },
+  --     { "<leader>lB", "<cmd>DBUIToggle<cr>", desc = "🔄 Toggle DBUI" },
+  --     { "<leader>la", "<cmd>DBUIAddConnection<cr>", desc = "➕ Add new DB connection" },
+  --   },
+  -- },
   {
     "nvzone/volt",
     { "nvzone/timerly", cmd = "TimerlyToggle" },
