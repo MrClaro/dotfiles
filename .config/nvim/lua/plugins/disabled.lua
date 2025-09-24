536b1696fb7c0086dbb934c8e26f37b6ba4c5942
MrClaro <adryan.contatoprofissional@gmail.com>
2025-09-24 11:09:02 -0300

    feat(plugin): criado arquivo para desabilitar plugins já não utilizados

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "hrsh7th/cmp-path", enabled = false },
+  { "saadparwaiz1/cmp_luasnip", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "andweeb/presence.nvim", enabled = false },
+}
